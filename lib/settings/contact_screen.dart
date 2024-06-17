14bb8bd0cf9889459378e8849a97db65f20d69ba
shinjxtsu <alessandroneri962@gmail.com>
2024-06-17 15:10:13 -0300

    added (placeholder) contact info text

--- lib/settings/contact_screen.dart
+++ lib/settings/contact_screen.dart
@@ -13,8 +13,25 @@ class ContactScreen extends StatelessWidget {
         backgroundColor: LightModeBackgroundColor,
       ),
       body: const Center(
-        child: Text('Contact Screen Body'),
+        child: Padding(
+          padding: EdgeInsets.all(16.0), // Add margin
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'The Flux app is developed and maintained by a student duo from the Pontifical Catholic University of Minas Gerais, Brazil. '
+                'For any bug reports, suggestions, or other reasons why one might '
+                'want to contact us, you may do so through either our mail at '
+                'fluxappdev@gmail.com or through our profile on X (formerly Twitter) '
+                'at @fluxfp',
+                textAlign: TextAlign.justify, // Center align the text
+                style: TextStyle(fontSize: 16.0), // Adjust font size if necessary
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
 }
+
